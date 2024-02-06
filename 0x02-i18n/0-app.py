c9b5fabf9cf410c0c12984d631a7f936b66fbc4a
BcodezMUJJE <billbrightmatege@gmail.com>
2024-02-06 19:28:25 +0000

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+ #!/usr/bin/env python3
+
+
+'''
+Task 0: Basic Flask app
+'''
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    '''the default route'''
+    return render_template("0-index.html",)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
